f0846b4ae45528e7e4848a329d6ceb200e17b078
AliaksandrAnashkevich <alex.anashkevich@gmail.com>
2022-02-14 19:20:53 +0300

    LA10.2: remove soft delete methods

--- src/main/java/com/academia/library/repository/OrderDetailsRepository.java
+++ src/main/java/com/academia/library/repository/OrderDetailsRepository.java
@@ -2,13 +2,6 @@ package com.academia.library.repository;
 
 import com.academia.library.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface OrderDetailsRepository extends JpaRepository<OrderDetail, Long> {
-
-    @Modifying
-    @Query(value = "DELETE FROM order_details o where o.deleted = true"
-            , nativeQuery = true)
-    void deleteAllByDeleted();
 }
--- src/main/java/com/academia/library/repository/OrderRepository.java
+++ src/main/java/com/academia/library/repository/OrderRepository.java
@@ -2,15 +2,8 @@ package com.academia.library.repository;
 
 import com.academia.library.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
-    @Modifying
-    @Query(value = "DELETE FROM orders o where o.deleted = true"
-            , nativeQuery = true)
-    void deleteAllByDeleted();
 }
